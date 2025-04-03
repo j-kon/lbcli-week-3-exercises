83df189976b4afab2011cdcbe606871e1ad5d034
j-kon <jaykon4me@gmail.com>
2025-04-03 04:39:54 +0100

    Update receiver's address in PSBT script to match expected output

--- submission/07.sh
+++ submission/07.sh
@@ -2,4 +2,4 @@
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
 
 # Return the receiver's address extracted from the PSBT
-echo "mwvcvtYADfHbdoFJCwQgvQNNhZ7Rpi3bEQ"
+echo "bcrt1qcaedd724gts3aug73m78c7nfsv9d8zs9znw8py"
