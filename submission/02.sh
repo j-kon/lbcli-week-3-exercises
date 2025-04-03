bb455d71de4297cfe41e3ac647f3d873a55fd834
j-kon <jaykon4me@gmail.com>
2025-04-03 04:32:55 +0100

    Refactor address retrieval in multisig and transaction scripts
    - Updated the multisig address creation scripts to extract the address using `grep` and `cut` for improved clarity.
    - Modified the transaction decoding scripts to retrieve the transaction ID and receiver's address more efficiently.

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "label" "bech32")
-bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$SEGWIT_ADDR" | grep "pubkey" | cut -d':' -f2 | tr -d ' ,"'
+SEGWIT_ADDR=$(bitcoin-cli -regtest getnewaddress "label" "bech32")
+bitcoin-cli -regtest getaddressinfo "$SEGWIT_ADDR" | grep "pubkey" | cut -d':' -f2 | tr -d ' ,"'
