3cd6e5f7a2af2090c3bae3d3e04ce68774b7fb73
j-kon <jaykon4me@gmail.com>
2025-04-03 04:20:15 +0100

    Add wallet creation and address generation scripts
    - Added a script to create a wallet named "btrustwallet".
    - Implemented address generation for a native segwit address and retrieval of its public key.
    - Created a 1-of-4 P2SH multisig address using specified public keys.
    - Added a 2-of-3 P2SH multisig address creation script.
    - Included raw transaction creation and decoding for partially signed transactions.

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32"
+bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32") | grep pubkey
