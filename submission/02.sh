dc8b0470044a10a2cdf65cda7c44e49cd2096700
j-kon <jaykon4me@gmail.com>
2025-04-03 04:29:52 +0100

    Refactor segwit address generation in script
    - Updated the script to store the generated native segwit address in a variable for better readability.
    - Enhanced the public key retrieval process by using `cut` and `tr` for cleaner output.

--- submission/02.sh
+++ submission/02.sh
@@ -1,3 +1,3 @@
 # Create a native segwit address and get the public key from the address.
-bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32"
-bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo $(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "" "bech32") | grep pubkey
+SEGWIT_ADDR=$(bitcoin-cli -regtest -rpcwallet=btrustwallet getnewaddress "label" "bech32")
+bitcoin-cli -regtest -rpcwallet=btrustwallet getaddressinfo "$SEGWIT_ADDR" | grep "pubkey" | cut -d':' -f2 | tr -d ' ,"'
